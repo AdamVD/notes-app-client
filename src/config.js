11ae805a9c42c1f4b936cd331886888b16ec5322
Adam Del Rosso <avdonle@outlook.com>
2020-04-01 16:12:47 -0400

    Add and setup AWS amplify

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-2",
+      BUCKET: "scratch-note-attachments"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "https://on0hnuu17l.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_Ju4wEzoJS",
+      APP_CLIENT_ID: "3rqp9be8orhsegb0a1d1tq49gs",
+      IDENTITY_POOL_ID: "us-east-2:2563c918-ae2e-4784-8480-25e65433e26c"
+    }
+};
