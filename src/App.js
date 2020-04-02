128c15f43e7682d6e0321539c0c56f44a99c5b68
Adam Del Rosso <avdonle@outlook.com>
2020-04-02 14:11:25 -0400

    Check session for login on first app load

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import { Link } from "react-router-dom";
 import { Nav, Navbar, NavItem } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 import "./App.css"
 import Routes from "./Routes";
+import { Auth } from "aws-amplify";
 
 function App(props) {
+  const [isAuthenticating, setIsAuthenticating] = useState(true);
   const [isAuthenticated, userHasAuthenticated] = useState(false);
+
+  useEffect(() => {
+    onLoad();
+  }, []);
+
+  async function onLoad() {
+    try {
+      await Auth.currentSession();
+      userHasAuthenticated(true);
+    } catch (e) {
+      if (e !== 'No current user') {
+        alert(e);
+      }
+    }
+
+    setIsAuthenticating(false);
+  }
 
   function handleLogout() {
     userHasAuthenticated(false);
   }
 
   return (
+    !isAuthenticating &&
     <div className="App container">
       <Navbar fluid collapseOnSelect>
         <Navbar.Header>
