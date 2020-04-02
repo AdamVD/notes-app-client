12cd9d24cccbba65d81d4700d0c8284beccb35c6
Adam Del Rosso <avdonle@outlook.com>
2020-04-02 18:13:22 -0400

    Add loading icon to login button

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,11 +1,13 @@
-import React, { useState } from "react";
-import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import React, {useState} from "react";
+import {ControlLabel, FormControl, FormGroup} from "react-bootstrap";
 import "./Login.css";
-import { Auth } from "aws-amplify";
+import {Auth} from "aws-amplify";
+import LoaderButton from "../components/LoaderButton";
 
 export default function Login(props) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   function validateForm() {
     return email.length > 0 && password.length > 0;
@@ -13,40 +15,48 @@ export default function Login(props) {
 
   async function handleSubmit(event) {
     event.preventDefault();
+    setIsLoading(true);
 
     try {
       await Auth.signIn(email, password);
       props.userHasAuthenticated(true);
       props.history.push('/');
     } catch (e) {
+      setIsLoading(false);
       alert(e.message);  // TODO provide an integrated error message
     }
   }
 
   return (
-    <div className="Login">
-      <form onSubmit={handleSubmit}>
-        <FormGroup controlId="email" bsSize="large">
-          <ControlLabel>Email</ControlLabel>
-          <FormControl
-            autoFocus
-            type="email"
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-          />
-        </FormGroup>
-        <FormGroup controlId="password" bsSize="large">
-          <ControlLabel>Password</ControlLabel>
-          <FormControl
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-            type="password"
-          />
-        </FormGroup>
-        <Button block bsSize="large" disabled={!validateForm()} type="submit">
-          Login
-        </Button>
-      </form>
-    </div>
+      <div className="Login">
+        <form onSubmit={handleSubmit}>
+          <FormGroup controlId="email" bsSize="large">
+            <ControlLabel>Email</ControlLabel>
+            <FormControl
+                autoFocus
+                type="email"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+            />
+          </FormGroup>
+          <FormGroup controlId="password" bsSize="large">
+            <ControlLabel>Password</ControlLabel>
+            <FormControl
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+                type="password"
+            />
+          </FormGroup>
+          <LoaderButton
+            block
+            type="submit"
+            bsSize="large"
+            isLoading={isLoading}
+            disabled={!validateForm()}
+          >
+            Login
+          </LoaderButton>
+        </form>
+      </div>
   );
 }
